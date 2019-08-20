348952bed0bec54d1e5c96fc028a29161562519c
justinjoco <justin.joco@gmail.com>
2019-08-20 14:59:12 -0700

    fixed carousel centering

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Carousel, Image, Button} from "react-bootstrap";
+import {Container, Carousel, Image, Button} from "react-bootstrap";
 import background_mac from "./assets/background_mac.jpg"
 
 const Home = () =>{
@@ -15,24 +15,25 @@ const Home = () =>{
 
     };
 
-    return ([<Carousel controls={false} indicators={false}>
-          <Carousel.Item>
-            <Image fluid src={background_mac} alt="Logo"
-            />
-            <Carousel.Caption style={textStyle}>
-                <h1 className="display-1" style={{fontSize: "10vmin"}}>Justin Joco</h1>
-                      <h3 style={{fontSize: "4vmin"}}>Developer, Learner, and Leader</h3>
-                     
-                    
-                      <Button variant="dark" href="#about" style={{margin: "5px"}}>About</Button>
-                      <Button variant="danger" href="https://drive.google.com/open?id=1N8Q8KU1-OL6Tt9YIO3bMyirn7EuZHyHd" style={{margin: "5px"}}>Resume</Button>
-                    
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>,
-    
-
-    ]);
+    return (
+      <Container  fluid style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Carousel controls={false} indicators={false} >
+            <Carousel.Item >
+              <Image fluid src={background_mac} alt="Logo"/>
+              <Carousel.Caption style={textStyle}>
+                  <h1 className="display-1" style={{fontSize: "10vmin"}}>Justin Joco</h1>
+                        <h3 style={{fontSize: "4vmin"}}>Developer, Learner, and Leader</h3>
+                       
+                      
+                        <Button variant="dark" href="#about" style={{margin: "5px"}}>About</Button>
+                        <Button variant="danger" href="https://drive.google.com/open?id=1N8Q8KU1-OL6Tt9YIO3bMyirn7EuZHyHd" style={{margin: "5px"}}>Resume</Button>
+                      
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+      </Container>
+     
+    );
 
 
 		 
